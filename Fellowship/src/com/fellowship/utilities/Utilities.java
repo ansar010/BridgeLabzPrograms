1ca4d840620e3d13fe57fb3638c078dc248e4ae5
ansar010 <ansaruddeen030@gmail.com>
2018-12-15 15:40:00 +0530

    New First Commit

---
+++ Fellowship/src/com/fellowship/utilities/Utilities.java
@@ -0,0 +1,98 @@
+package com.fellowship.utilities;
+import java.util.Scanner;
+
+public class Utilities {
+
+	static Scanner sc = new Scanner(System.in);
+
+	//Method to replace String
+	/**
+	 * @param To replace string 
+	 */
+	static public void replaceString(String username)
+	{
+		// tmpl_Str refers template
+		String tmpl_Str = "Hello <<UserName>>, How are you?";
+
+		//To ensure user-name has minimum 3 char
+		if(username.length()<3)
+		{
+			System.err.println("Username should has minimum 3 char");	
+		}
+		else
+		{
+			//temp refers to temporary variable
+			String temp="";
+			temp += tmpl_Str.substring(0,6);// substring() method used to get substring of template
+			temp += username;
+			temp += tmpl_Str.substring(18,tmpl_Str.length());
+			System.out.println(temp);
+		}
+	}
+	//********************************************************************************
+
+
+
+	//Method to Flip Coin and calculate percentage
+	/**
+	 * 
+	 * @param n Number of time to flip coin 
+	 */
+	public static void flipCoin(int n)
+	{
+		// response -> to get user choice  
+		int count=1,response;
+		double heads=0,tails=0;
+
+		do
+		{
+			System.out.println("\nEnter:\n 1-> To flip coin.\n 0-> To Exit.");
+			response=sc.nextInt();
+			if(response==1)
+			{
+				if(cointoss()==1)  // Calling method to Toss Coin
+					tails++;       //Increasing tails value to get count
+				else
+					heads++;       //Increasing heads value to get count 
+			}
+			else if(response==0)
+			{
+				System.out.println("You Quit the game..!");
+			}
+			else if(response>1)
+			{
+				System.out.println("Please Enter valid number 0 or 1");
+			}
+			count++;
+
+		}while(count<=n&&response!=0); //Condition to repeat loop 
+		//and exit game if user give 0
+		System.out.println("\nNumber of time Heads : "+heads);
+		System.out.println("Number of time Tails : "+tails);
+
+		double percentHeads=heads/n*100;     //Percentage of Heads
+		double percentTails = (tails/n*100); //Percentage of Tails
+
+		System.out.println("\nPercentage of Heads VS Tails");
+		System.out.println("____________________________\n\n");
+		System.out.println("Heads is : "+percentHeads+"%");
+		System.out.println("Tails is : "+percentTails+"%");
+		System.out.println("hihihi");
+	}
+
+	//Method to Toss Coin
+	public static int cointoss()
+	{
+		double result = Math.random();//generate random value
+		if(result<0.5)
+		{
+			System.out.println("You flipped Tails !");
+			return 1; 
+		}
+		else
+		{
+			System.out.println("You flipped Heads !");
+			return 0;
+		}
+	}
+}

25602a3f7f39ed47823466244503a5e2c0ca7f66
ansar010 <ansaruddeen030@gmail.com>
2018-12-17 19:22:44 +0530

    commit3

--- Fellowship/src/com/fellowship/utilities/Utilities.java
+++ Fellowship/src/com/fellowship/utilities/Utilities.java
@@ -126,11 +126,11 @@ public class Utilities {
 		 */
 			if(year%4==0&&year%100!=0||year%400==0) 
 			{
-				System.out.println(year+" is a leap year");
+				System.out.println(year+" is a leap year.");
 			}
 			else
 			{
-				System.out.println(year+" is not a leap year");
+				System.out.println(year+" is not a leap year.");
 			}
 		}
 	}
